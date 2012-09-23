f45afcf3a3442e4c3fd5ffe4bc57d1f2450ec268
Vladas Diržys <vladas.dirzys@gmail.com>
2012-09-23 23:46:30 +0200

    BUGFIX: Fixed to work with new security policy. Removed Iframe.

--- html5player.js
+++ html5player.js
@@ -1,20 +1,11 @@
 $(function(){
     var flashvars = $('#video_win object param[name=flashvars]').attr("value");
-//    console.log(flashvars);
     var pattern = /(src="|href="|">|\s>)?(https?|ftp):\/\/media[-A-Z0-9+&@#\/%?=~_|!:,.;ï]*[-A-Z0-9+&@#\/%=~_|ï]/gim;
     var matches = flashvars.match(pattern);
-//    console.log(matches);
     $('#video_win object').remove();
     
-    iframeUrl = chrome.extension.getURL('player.html') + "?src=" + matches[0]
-    
-    var el = document.createElement("iframe");
-    el.setAttribute('id', 'html5videoifrm');
-    el.setAttribute('src', iframeUrl);
-    el.setAttribute('height', 400);
-    el.setAttribute('width', 679);
-    el.setAttribute('scrolling', 'no');
-    el.setAttribute('frameborder', 0);
+    $('#video_win').prepend("<video id=\"html5video\" controls  preload=\"auto\" width=\"679\" height=\"400\" src=\"\"></video>");
+    $('#html5video').attr("src", matches[0]);
     
     $('#video_win').prepend(el);
 });
